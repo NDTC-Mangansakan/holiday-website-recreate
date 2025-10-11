9a3b71741d99ef679146c04fe3045b2c8e4fbae8
Shaq <mangansakansansudin@gmail.com>
2025-10-11 14:19:53 +0800

    added more height to about page

--- src/routes/main/subContent/About.jsx
+++ src/routes/main/subContent/About.jsx
@@ -5,7 +5,7 @@ const aboutLinks = [
 
 const About = () => {
     return (
-        <div className='md:col-span-2 min-h-[50vh] bg-primaryOrange px-5 py-3'>
+        <div className='md:col-span-2 min-h-[80vh] bg-primaryOrange px-5 py-3'>
             <p className="text-3xl font-extrabold lg:text-4xl xl:text-5xl">
                 <span className="inline-block bg-black w-5 h-5 rounded-full mr-3"></span>
                 A CREATIVE PRODUCTION HOUSE WITH A STRONG BELIEF IN THE POWER OF COLLABORATION. WE AIM TO SERVE EACH PROJECT UNIQUELY, ALL SCALE AND SCOPE, CREATING LASTING BONDS BETWEEN BRANDS, TALENTS AND DECORS. WITH A SOLID KNOWLEDGE OF EUROPEAN TERRITORIES AND WIDE-REACHING NETWORKS, OUR INTERNATIONAL CREW PROVIDES A LONG-STANDING EXPERTISE.
